dc96f4762e3de28c268da9a5eedf1c490f26a701
Berta Floran <bertafloran@hotmail.com>
2022-08-21 18:53:57 +0200

    create custom hook useRegister

---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,21 @@
+import { ProtoUser } from "../store/models/User";
+
+export const useRegister = () => {
+  const postRegister = async (newUser: ProtoUser) => {
+    const url = process.env.REACT_APP_API_URL as string;
+
+    const response = await fetch(`${url}user/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    const registerResponse: ProtoUser = await response.json();
+
+    return registerResponse;
+  };
+
+  return { postRegister };
+};
