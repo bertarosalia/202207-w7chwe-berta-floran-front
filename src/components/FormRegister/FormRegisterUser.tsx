bda79f81ddabf22a88b3e380e9b9a5727b12671a
Berta Floran <bertafloran@hotmail.com>
2022-08-21 19:27:00 +0200

    add functionality to register component

--- src/components/FormRegister/FormRegisterUser.tsx
+++ src/components/FormRegister/FormRegisterUser.tsx
@@ -1,12 +1,36 @@
+import { SyntheticEvent, useState } from "react";
+import useRegister from "../../hooks/useRegister";
+import { ProtoUser } from "../../store/models/User";
 import FormRegisterUserStyled from "./FormRegisterUserStyled";
 
 const RegisterUserForm = () => {
+  const initialUser: ProtoUser = {
+    userName: "",
+    password: "",
+  };
+
+  const [userRegisterData, setUserRegisterData] = useState(initialUser);
+  const { postRegister } = useRegister();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserRegisterData({
+      ...userRegisterData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    postRegister(userRegisterData);
+    setUserRegisterData(initialUser);
+  };
+
   return (
     <>
       <FormRegisterUserStyled>
         <div className="register-container">
           <h1 className="register-title">Register form</h1>
-          <form className="register-form">
+          <form className="register-form" onSubmit={handleSubmit}>
             <div className="form-inputs">
               <label className="name">Name</label>
               <input
@@ -15,6 +39,8 @@ const RegisterUserForm = () => {
                 placeholder="userName"
                 required
                 autoComplete="off"
+                value={userRegisterData.userName}
+                onChange={handleChange}
               />
             </div>
             <div className="form-inputs">
@@ -26,6 +52,8 @@ const RegisterUserForm = () => {
                 placeholder="password"
                 required
                 autoComplete="off"
+                value={userRegisterData.password}
+                onChange={handleChange}
               />
             </div>
             <button className="register" type="submit">
